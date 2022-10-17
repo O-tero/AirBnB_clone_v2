8070e84e6733ca41b13e9675016281d7106e36d6
O-tero <oteronickbrian@gmail.com>
2022-10-17 23:48:19 +0300

    AirBnB clone - Web framework

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
+"""Starts a flask app
+    listens to 0.0.0.0:5000
+    
 """
-Script that starts a Flask web application
-/states_list: display a HTML page: (inside the tag BODY) 
-"""
-from flask import Flask, render_template
-from models import storage, State
+from models import storage
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """Displays an HTML page with a list of all State objects in DBStorage.
+    States are sorted by name.
+    """
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
-def close_context(exception):
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
-@app.route('/states_list')
-def states_route():
-    states = storage.all(State)
-    all_states = []
-
-    for state in states.values():
-        all_states.append([state.id, state.name])
-    return render_template('7-states_list.html', states=all_states)
-
-
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0")
